a02696468b8ecd51770d76f2191c80bd126d4120
lzw <1096693846@qq.com>
2024-09-22 22:18:05 +0800

    空白模板

---
+++ encode/encode.cpp
@@ -0,0 +1,15 @@
+﻿#include "encode.h"
+
+extern "C"
+{
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libavutil/avutil.h>
+#include <libavutil/imgutils.h>
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
